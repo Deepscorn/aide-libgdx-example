bac15c3dada88d34fa99de76308f55cf2f535f1c
mGelashvili13 <mgelashvili@mail.ru>
2016-06-28 23:08:58 +0300

    init

---
+++ desktop/src/main/java/com/deepscorn/koten/desktop/DesktopLauncher.java
@@ -0,0 +1,28 @@
+package com.deepscorn.koten.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.deepscorn.koten.Main;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static LwjglApplication createApplication() {
+        return new LwjglApplication(new Main(), getDefaultConfiguration());
+    }
+
+    private static LwjglApplicationConfiguration getDefaultConfiguration() {
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.title = "Koten";
+        configuration.width = 640;
+        configuration.height = 480;
+        for (int size : new int[] { 128, 64, 32, 16 }) {
+            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+        }
+        return configuration;
+    }
+}
