d42f6e7013475e7f2a9f4271c4c9ebaf6a75f015
Deepscorn <vnms11@gmail.com>
2016-07-03 18:48:24 +0300

    show image

--- core/src/main/java/com/deepscorn/koten/FirstScreen.java
+++ core/src/main/java/com/deepscorn/koten/FirstScreen.java
@@ -1,17 +1,32 @@
 package com.deepscorn.koten;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    private Texture backgroundTexture;
+    private SpriteBatch batch;
+
     @Override
     public void show() {
         // Prepare your screen here.
+        backgroundTexture = new Texture(Gdx.files.internal("libgdx128.png"));
+        batch = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0);
+        batch.end();
     }
 
     @Override
@@ -37,5 +52,7 @@ public class FirstScreen implements Screen {
     @Override
     public void dispose() {
         // Destroy screen's assets here.
+        backgroundTexture.dispose();
+        batch.dispose();
     }
 }
